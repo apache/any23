4ef1f1f3fa3aa5e9bfb7ac95cc77d49e89e33cb8
juergen@umbrich.net <dev-null@apache.org>
2009-02-06 18:29:38 +0000

    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223088 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/org/deri/any23/cli/Main.java
+++ library/src/org/deri/any23/cli/Main.java
@@ -1,40 +1,79 @@
 package org.deri.any23.cli;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
 
 public class Main {
 	private static final String USAGE = " <utility> [options...]";
 	private static final String PREFIX = "org.deri.any23.cli.";
 
-	public static void main(String[] args) {		
+	public static void main(String[] args) {	
+		//generate automatically the cli
+		List<String> utilities = getClasseNamesInPackage(args[0], "org.deri.any23.cli");
+		
 		try {
-			if (args.length < 1) {			
+			if (args.length < 2) {			
 				StringBuffer sb = new StringBuffer();
-				sb.append("where <utility> one of");
-				sb.append("\n\t"+Rover.class.getSimpleName());
+				sb.append(" where <utility> one of");
+				for(String util: utilities)
+					sb.append("\n\t"+util);
 				usage(sb.toString());
 			}
 			
-			Class cls = Class.forName(PREFIX + args[0]);
+			Class cls = Class.forName(PREFIX + args[1]);
 			
 			Method mainMethod = cls.getMethod("main", new Class[] { String[].class });
 
-			String[] mainArgs = new String[args.length - 1];
-			System.arraycopy(args, 1, mainArgs, 0, mainArgs.length);
-			
-			long time = System.currentTimeMillis();
+			String[] mainArgs = new String[args.length - 2];
+			System.arraycopy(args, 2, mainArgs, 0, mainArgs.length);
 			
 			mainMethod.invoke(null, new Object[] { mainArgs });
-			
-			long time1 = System.currentTimeMillis();
-			
-			System.err.println("time elapsed " + (time1-time) + " ms");
 		} catch (Throwable e) {
 			e.printStackTrace();
 			Throwable cause = e.getCause();
 			cause.printStackTrace();
 			usage(e.toString());
 		}
+	}
+
+	/**http://www.rgagnon.com/javadetails/java-0513.html **/
+	public static List<String> getClasseNamesInPackage   (String jarName, String packageName){
+		boolean debug = true;
+		ArrayList<String> classes = new ArrayList<String>();
+		packageName = packageName.replaceAll("\\." , "/");
+		try{
+			 if (debug) System.err.println
+		        ("Jar " + jarName + " looking for " + packageName);
+
+			JarInputStream jarFile = new JarInputStream(new FileInputStream (jarName));
+			JarEntry jarEntry;
+
+			while(true) {
+				jarEntry=jarFile.getNextJarEntry ();
+				if(jarEntry == null){
+					break;
+				}
+				if((jarEntry.getName ().startsWith (packageName)) &&
+						(jarEntry.getName ().endsWith (".class")) ) {
+					String classEntry = jarEntry.getName().replaceAll("/", "\\.");
+					 if (debug) System.err.println 
+			           ("Found " + jarEntry.getName().replaceAll("/", "\\."));
+
+					
+					classes.add(classEntry.substring("org.deri.any23.cli.".length(),classEntry.indexOf(".class") ));
+				}
+			}
+		}
+		catch( Exception e){
+			e.printStackTrace ();
+		}
+		return classes;
 	}
 
 	private static void usage(String msg) {
