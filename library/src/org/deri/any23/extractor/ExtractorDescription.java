2d19f28843c57407f21644be4bb04ae2a3ca0c13
pirroh <dev-null@apache.org>
2009-04-07 12:17:05 +0000

    got rid of directories used before the Maven-hierarchy switch
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223132 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/org/deri/any23/extractor/ExtractorDescription.java
+++
@@ -1,8 +0,0 @@
-package org.deri.any23.extractor;
-
-import org.deri.any23.rdf.Prefixes;
-
-public interface ExtractorDescription {
-	String getExtractorName();
-	Prefixes getPrefixes();
-}
--- library/src/org/deri/any23/writer/FormatWriter.java
+++
@@ -1,5 +0,0 @@
-package org.deri.any23.writer;
-
-public interface FormatWriter extends TripleHandler {
-	String getMIMEType();
-}
