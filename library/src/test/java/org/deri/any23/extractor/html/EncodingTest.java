49a7dfafcef4ec5aa883b7c2ea21013bd99d2999
richard@cyganiak.de <dev-null@apache.org>
2009-09-07 19:55:05 +0000

    Documented known issue with charset detection in NekoHTML, and upgraded to NekoHTML 1.9.13
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223224 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/test/java/org/deri/any23/extractor/html/EncodingTest.java
+++ library/src/test/java/org/deri/any23/extractor/html/EncodingTest.java
@@ -1,11 +1,8 @@
 package org.deri.any23.extractor.html;
 
-import org.deri.any23.extractor.html.HTMLDocument;
-import org.junit.Test;
-
-
-
 import junit.framework.TestCase;
+
+import org.junit.Test;
 
 public class EncodingTest extends TestCase {
 	private final static String HELLO_WORLD = "Hell\u00F6 W\u00F6rld!";
@@ -20,6 +17,17 @@ public class EncodingTest extends TestCase {
 	public void testEncodingHTML_UTF_8() {
 		HTMLDocument document = parseHTML("xfn/encoding-utf-8.html");
 		assertEquals(HELLO_WORLD, document.find("//TITLE"));
+	}
+
+	@Test
+	public void testEncodingHTML_UTF_8_DeclarationAfterTitle() {
+// Known issue: NekoHTML does not auto-detect the encoding, but relies
+// on the explicitly specified encoding (via XML declaration or
+// HTTP-Equiv meta header). If the meta header comes *after* the
+// title element, then NekoHTML will not use the declared encoding
+// for the title.
+//		HTMLDocument document = parseHTML("xfn/encoding-utf-8-after-title.html");
+//		assertEquals(HELLO_WORLD, document.find("//TITLE"));
 	}
 
 	@Test
