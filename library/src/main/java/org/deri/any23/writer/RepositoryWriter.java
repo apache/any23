47612563e67b9e33d3d6e1c98ed085906e60c186
richard@cyganiak.de <dev-null@apache.org>
2009-09-08 19:22:33 +0000

    Add a constructor for overriding the OpenRDF context where triples are stored
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223229 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/main/java/org/deri/any23/writer/RepositoryWriter.java
+++ library/src/main/java/org/deri/any23/writer/RepositoryWriter.java
@@ -9,9 +9,15 @@ import org.openrdf.repository.RepositoryException;
 
 public class RepositoryWriter implements TripleHandler {
 	private final RepositoryConnection conn;
+	private final Resource overrideContext;
 	
 	public RepositoryWriter(RepositoryConnection conn) {
+		this(conn, null);
+	}
+	
+	public RepositoryWriter(RepositoryConnection conn, Resource overrideContext) {
 		this.conn = conn;
+		this.overrideContext = overrideContext;
 	}
 	
 	public void startDocument(URI documentURI) {
@@ -25,7 +31,7 @@ public class RepositoryWriter implements TripleHandler {
 	public void receiveTriple(Resource s, URI p, Value o,
 			ExtractionContext context) {
 		try {
-			conn.add(conn.getValueFactory().createStatement(s, p, o), context.getDocumentURI());
+			conn.add(conn.getValueFactory().createStatement(s, p, o), getContextResource(context.getDocumentURI()));
 		} catch (RepositoryException ex) {
 			throw new RuntimeException(ex);
 		}
@@ -59,5 +65,12 @@ public class RepositoryWriter implements TripleHandler {
 //		_contentLength = contentLength;
 		//ignore
 		;
+	}
+	
+	private Resource getContextResource(Resource fromExtractor) {
+		if (overrideContext != null) {
+			return overrideContext;
+		}
+		return fromExtractor;
 	}
 }
