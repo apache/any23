c6d0f6fa2c9e0083c250d99eb7a976a387bd457f
michele.mostarda <dev-null@apache.org>
2010-02-03 16:58:40 +0000

    TODO has been associated to an issue. Code prettification.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223282 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/main/java/org/deri/any23/rdf/PopularPrefixes.java
+++ library/src/main/java/org/deri/any23/rdf/PopularPrefixes.java
@@ -19,27 +19,26 @@ package org.deri.any23.rdf;
 /**
  * This class act as a container for various well-known and adopted RDF Vocabulary prefixes.
  */
-// TODO (high): this way of hardcoding prefixes in a class is an anti-pattern: must try a more flexible solution.
+// TODO: #4 - this way of hardcoding prefixes in a class is an anti-pattern: must try a more flexible solution.
 public class PopularPrefixes {
 
     private final static Prefixes popularPrefixes = new Prefixes() {
         {
-            add("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
-            add("rdfs", "ttp://www.w3.org/2000/01/rdf-schema#");
-            add("xhtml", "http://www.w3.org/1999/xhtml/vocab#");
-            add("dcterms", "http://purl.org/dc/terms/");
-            add("foaf", "http://xmlns.com/foaf/0.1/");
-            add("geo", "http://www.w3.org/2003/01/geo/wgs84_pos#");
-            add("xfn", "http://vocab.sindice.com/xfn#");
-            add("vcard", "http://www.w3.org/2006/vcard/ns#");
-            add("ical", "http://www.w3.org/2002/12/cal/icaltzd#");
-            add("hlisting", "http://sindice.com/hlisting/0.1/");
-            add("rev", "http://purl.org/stuff/rev#");
-            add("doac", "http://ramonantonio.net/doac/0.1/#");
-            add("ex", "http://example.com/ns#");
+            add("rdf"     , "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+            add("rdfs"    , "ttp://www.w3.org/2000/01/rdf-schema#"       );
+            add("xhtml"   , "http://www.w3.org/1999/xhtml/vocab#"        );
+            add("dcterms" , "http://purl.org/dc/terms/"                  );
+            add("foaf"    , "http://xmlns.com/foaf/0.1/"                 );
+            add("geo"     , "http://www.w3.org/2003/01/geo/wgs84_pos#"   );
+            add("xfn"     , "http://vocab.sindice.com/xfn#"              );
+            add("vcard"   , "http://www.w3.org/2006/vcard/ns#"           );
+            add("ical"    , "http://www.w3.org/2002/12/cal/icaltzd#"     );
+            add("hlisting", "http://sindice.com/hlisting/0.1/"           );
+            add("rev"     , "http://purl.org/stuff/rev#"                 );
+            add("doac"    , "http://ramonantonio.net/doac/0.1/#"         );
+            add("ex"      , "http://example.com/ns#"                     );
         }
     };
-
 
     /**
      *
