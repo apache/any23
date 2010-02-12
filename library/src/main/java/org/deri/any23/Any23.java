1b709ec4ed0024acd2554f21a69e372f82e9a77f
michele.mostarda <dev-null@apache.org>
2010-02-12 16:30:27 +0000

    Updated to release 0.2 WOW
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223381 13f79535-47bb-0310-9956-ffa450edef68

--- library/src/main/java/org/deri/any23/Any23.java
+++ library/src/main/java/org/deri/any23/Any23.java
@@ -54,7 +54,7 @@ import java.util.Collection;
 public class Any23 {
 
     // NOTE: there's also a version string in build.xml, they should match.
-    public static final String VERSION = "0.2-dev";
+    public static final String VERSION = "0.2";
 
     private final ExtractorGroup factories;
     private LocalCopyFactory streamCache;
