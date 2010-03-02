bc033393a9a2f09bafdb6479c3cebbe8fd2ded2f
michele.mostarda <dev-null@apache.org>
2010-03-02 14:00:24 +0000

    Defined bug fixing release 0.2.2 .
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223463 13f79535-47bb-0310-9956-ffa450edef68

--- any23-core/src/main/java/org/deri/any23/Any23.java
+++ any23-core/src/main/java/org/deri/any23/Any23.java
@@ -54,7 +54,7 @@ import java.util.Collection;
 public class Any23 {
 
     // NOTE: there's also a version string in build.xml and pom.xml, they should match.
-    public static final String VERSION = "0.2.1";
+    public static final String VERSION = "0.2.2";
 
     private final ExtractorGroup factories;
     private LocalCopyFactory streamCache;
