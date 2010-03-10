7d41afc4229a1f67652024e71aec6e405c8d4e78
michele.mostarda <dev-null@apache.org>
2010-03-10 17:09:09 +0000

    Version 0.3.0-dev has been defined.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223465 13f79535-47bb-0310-9956-ffa450edef68

--- any23-core/src/main/java/org/deri/any23/Any23.java
+++ any23-core/src/main/java/org/deri/any23/Any23.java
@@ -54,7 +54,7 @@ import java.util.Collection;
 public class Any23 {
 
     // NOTE: there's also a version string in build.xml and pom.xml, they should match.
-    public static final String VERSION = "0.2.2";
+    public static final String VERSION = "0.3.0-dev";
 
     private final ExtractorGroup factories;
     private LocalCopyFactory streamCache;
