5198608a862b8b8fe76196d01a52847a1a0802e8
michele.mostarda <dev-null@apache.org>
2010-05-21 16:27:09 +0000

    javadoc has been fixed.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1223590 13f79535-47bb-0310-9956-ffa450edef68

---
+++ any23-core/src/main/java/org/deri/any23/validator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package defines a simple rule based validation
+ * framework able to detect and fix common mistakes that
+ * can be present within an <i>HTML</i> DOM model and
+ * that can prevent the extraction of metadata.
+ */
+package org.deri.any23.validator;
---
+++ any23-core/src/main/java/org/deri/any23/validator/rule/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * A set of predefined validation {@link org.deri.any23.validator.Rule}s
+ * and associated {@link org.deri.any23.validator.Fix}es. 
+ */
+package org.deri.any23.validator.rule;
