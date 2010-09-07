e45cf93294322495f5b4cc356ab25910e4269348
michele.mostarda <dev-null@apache.org>
2010-09-07 12:55:48 +0000

    Switched trunk to 0.4.2 snapshot.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1224011 13f79535-47bb-0310-9956-ffa450edef68

--- any23-core/src/main/java/org/deri/any23/Any23.java
+++ any23-core/src/main/java/org/deri/any23/Any23.java
@@ -57,7 +57,7 @@ import java.util.Collection;
 public class Any23 {
 
     // NOTE: there's also a version string in pom.xml, they should match.
-    public static final String VERSION = "0.5.0-SNAPSHOT";
+    public static final String VERSION = "0.4.2-SNAPSHOT";
 
     private final ExtractorGroup factories;
     private LocalCopyFactory streamCache;
