a3d367ae7171d2a7050ce1ee0d7410b786f0df9a
michele.mostarda <dev-null@apache.org>
2011-05-29 10:12:33 +0000

    Added missing package descriptor.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1224167 13f79535-47bb-0310-9956-ffa450edef68

---
+++ plugins/html-scraper/src/main/java/org/deri/any23/plugin/htmlscraper/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This module contains the <i>Any23 HTMLScraper</i> plugin.
+ * Such plugin provides the {@link HTMLScraperExtractor}, an
+ * extractor able to return significant text from any <i>HTML</i> page.
+ */
+package org.deri.any23.plugin.htmlscraper;
