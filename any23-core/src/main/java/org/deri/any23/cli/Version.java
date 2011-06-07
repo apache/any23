ac65b54b5bb4a8194d6ca4de9bf4dcd96bc53775
michele.mostarda <dev-null@apache.org>
2011-06-07 22:00:05 +0000

    Added Version tool.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1224267 13f79535-47bb-0310-9956-ffa450edef68

---
+++ any23-core/src/main/java/org/deri/any23/cli/Version.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2008-2010 Digital Enterprise Research Institute (DERI)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *          http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.deri.any23.cli;
+
+import org.deri.any23.Configuration;
+
+/**
+ * Prints out the <b>Any23</b> library version.
+ *
+ * @author Michele Mostarda (mostarda@fbk.eu)
+ */
+@ToolRunner.Description("Prints out the current library version and configuration information.")
+public class Version {
+
+    public static void main(String[] args) {
+        final String version = Configuration.instance().getProperty("any23.core.version", null);
+        if(version == null) {
+            System.err.println("Error while retrieving configuration info.");
+            System.exit(1);
+        }
+        System.out.println(String.format("Any23 Core v. %s", version));
+        System.out.println();
+        System.exit(0);
+    }
+
+}
