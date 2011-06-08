8ccdc0c6dad13f885113ff4975829d7605403830
michele.mostarda <dev-null@apache.org>
2011-06-08 08:46:05 +0000

    Added commandline Microdata parser. This commit is related to issue #114.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1224270 13f79535-47bb-0310-9956-ffa450edef68

---
+++ any23-core/src/main/java/org/deri/any23/cli/MicrodataParser.java
@@ -0,0 +1,64 @@
+package org.deri.any23.cli;
+
+import org.deri.any23.extractor.html.TagSoupParser;
+import org.deri.any23.extractor.microdata.MicrodataUtils;
+import org.deri.any23.http.DefaultHTTPClient;
+import org.deri.any23.source.DocumentSource;
+import org.deri.any23.source.FileDocumentSource;
+import org.deri.any23.source.HTTPDocumentSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Command line <b>Microdata</i> parser, accepting both files and URLs and
+ * returing a <i>JSON</i> representation of the extracted metadata.
+ *
+ * @author Michele Mostarda (mostarda@fbk.eu)
+ */
+@ToolRunner.Description("Commandline Tool for extracting Microdata from file/HTTP source.")
+public class MicrodataParser {
+
+    private static final String HTTP_DOCUMENT_SOURCE = "^https?://.*";
+    private static final String FILE_DOCUMENT_SOURCE = "^file:(.*)$";
+
+    public static void main(String[] args) throws URISyntaxException, IOException {
+        if(args.length != 1) {
+            System.err.println("USAGE: {http://path/to/resource.html|file:/path/to/local.file}");
+            System.exit(1);
+        }
+        InputStream documentInputInputStream = null;
+        try {
+            final DocumentSource documentSource = getDocumentSource(args[0]);
+            documentInputInputStream = documentSource.openInputStream();
+            final TagSoupParser tagSoupParser = new TagSoupParser(
+                    documentInputInputStream,
+                    documentSource.getDocumentURI()
+            );
+            MicrodataUtils.getMicrodataAsJSON(tagSoupParser.getDOM(), System.out);
+        } catch (Exception e) {
+            System.err.println("***ERROR: " + e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+        } finally {
+            if(documentInputInputStream != null) documentInputInputStream.close();
+        }
+        System.exit(0);
+    }
+
+    private static DocumentSource getDocumentSource(String source) throws URISyntaxException {
+        final Matcher httpMatcher = Pattern.compile(HTTP_DOCUMENT_SOURCE).matcher(source);
+        if(httpMatcher.find()) {
+            return new HTTPDocumentSource(new DefaultHTTPClient(), source);
+        }
+        final Matcher fileMatcher = Pattern.compile(FILE_DOCUMENT_SOURCE).matcher(source);
+        if(fileMatcher.find()) {
+            return new FileDocumentSource( new File( fileMatcher.group(1) ) );
+        }
+        throw new IllegalArgumentException("Invalid source protocol: '" + source + "'");
+    }
+}
