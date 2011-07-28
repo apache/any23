d4c20e8d34b0020d88edafff3d6c8269422ecb61
michele.mostarda <dev-null@apache.org>
2011-07-28 13:18:33 +0000

    Added CSVReaderBuilder#isCSV verification method. Added CSVReaderBuilderTest. This commit is related to issue #176.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1224478 13f79535-47bb-0310-9956-ffa450edef68

--- any23-core/src/main/java/org/deri/any23/extractor/csv/CSVReaderBuilder.java
+++ any23-core/src/main/java/org/deri/any23/extractor/csv/CSVReaderBuilder.java
@@ -29,6 +29,7 @@ import java.io.InputStreamReader;
  * from the file it self and then, if not successful, from the {@link DefaultConfiguration}.
  *
  * @author Davide Palmisano ( dpalmisano@gmail.com )
+ * @author Michele Mostarda ( michele.mostarda@gmail.com )
  */
 public class CSVReaderBuilder {
 
@@ -38,10 +39,21 @@ public class CSVReaderBuilder {
 
     public static final char NULL_CHAR = ' ';
 
-    private static final String[] popularDelimiters = {"\t", "|", ",", ";"};
+    private static final char[] popularDelimiters = {'\t', '|', ',', ';'};
 
     private static DefaultConfiguration defaultConfiguration =
             DefaultConfiguration.singleton();
+
+    private static final CSVStrategy[] strategies;
+
+    static {
+        strategies = new CSVStrategy[ popularDelimiters.length + 1 ];
+        strategies[0] = CSVStrategy.DEFAULT_STRATEGY;
+        int index = 1;
+        for(char dlmt : popularDelimiters) {
+            strategies[index++] = getCsvStrategy(dlmt, NULL_CHAR);
+        }
+    }
 
     /**
      * Builds a not <code>null</code> {@link org.apache.commons.csv.CSVParser} guessing
@@ -49,44 +61,32 @@ public class CSVReaderBuilder {
      *
      * @param is {@link InputStream} of the <i>CSV</i> file where guess the configuration.
      * @return a {@link CSVParser}
+     * @throws java.io.IOException
      */
     public static CSVParser build(InputStream is) throws IOException {
-        CSVParser parser;
-        // now try real CSV reader with popular delimiters, starting with pure TAB and classic CSV first:
-        is.mark(0);
-        parser = new CSVParser(
-                new InputStreamReader(is),
-                CSVStrategy.DEFAULT_STRATEGY
-        );
-        is.reset();
-        if (testParser(parser)) {
-            is.reset();
-            return new CSVParser(
-                    new InputStreamReader(is),
-                    CSVStrategy.DEFAULT_STRATEGY
-            );
-        }
-        // TAB and others
-        for (String dlmt : popularDelimiters) {
-            parser = new CSVParser(
-                    new InputStreamReader(is),
-                    getCsvStrategy(dlmt.charAt(0), NULL_CHAR)
-            );
-            is.reset();
-            if (testParser(parser)) {
-                is.reset();
-                return new CSVParser(
-                        new InputStreamReader(is),
-                        getCsvStrategy(dlmt.charAt(0), NULL_CHAR)
-                );
+        CSVStrategy bestStrategy = getBestStrategy(is);
+        if(bestStrategy == null) bestStrategy = getCSVStrategyFromConfiguration();
+        return new CSVParser( new InputStreamReader(is), bestStrategy );
+    }
+
+    /**
+     * Checks whether the given input stream is a CSV or not.
+     *
+     * @param is input stream to be verified.
+     * @return
+     * @throws IOException
+     */
+    public static boolean isCSV(InputStream is) throws IOException {
+        return getBestStrategy(is) != null;
+    }
+
+    private static CSVStrategy getBestStrategy(InputStream is) throws IOException {
+        for( CSVStrategy strategy : strategies ) {
+            if( testStrategy(is, strategy) ) {
+                return strategy;
             }
         }
-        // if is not possible to detect them, then build it from configuration
-        is.reset();
-        return new CSVParser(
-                new InputStreamReader(is),
-                getCSVStrategyFromConfiguration()
-        );
+        return null;
     }
 
     private static CSVStrategy getCsvStrategy(char delimiter, char comment) {
@@ -120,38 +120,44 @@ public class CSVReaderBuilder {
      * make sure the reader has correct delimiter and quotation set.
      * Check first lines and make sure they have the same amount of columns and at least 2
      *
-     * @param parser
+     * @param is input stream to be checked
+     * @param strategy strategy to be verified.
      * @return
      * @throws IOException
+     * @param is
      */
-    private static boolean testParser(CSVParser parser) throws IOException {
-        final int minColumns = 2;
-        int linesToCheck = 25;
+    private static boolean testStrategy(InputStream is, CSVStrategy strategy) throws IOException {
+        final int MIN_COLUMNS = 2;
 
-        int headerColumnCount = 0;
-        while (linesToCheck > 0) {
-            String[] row;
-            row = parser.getLine();
-            if (row == null) {
-                break;
-            }
-            if (row.length < minColumns) {
-                return false;
-            }
-            if (headerColumnCount == 0) {
-                // first row
-                headerColumnCount = row.length;
-            } else {
-                // make sure rows have the same number of columns or one more than the header
-                if (row.length < headerColumnCount) {
-                    return false;
-                } else if (row.length - 1 > headerColumnCount) {
+        is.mark(Integer.MAX_VALUE);
+        try {
+            final CSVParser parser = new CSVParser(new InputStreamReader(is), strategy);
+            int linesToCheck = 5;
+            int headerColumnCount = -1;
+            while (linesToCheck > 0) {
+                String[] row;
+                row = parser.getLine();
+                if (row == null) {
+                    break;
+                }
+                if (row.length < MIN_COLUMNS) {
                     return false;
                 }
+                if (headerColumnCount == -1) { // first row
+                    headerColumnCount = row.length;
+                } else { // make sure rows have the same number of columns or one more than the header
+                    if (row.length < headerColumnCount) {
+                        return false;
+                    } else if (row.length - 1 > headerColumnCount) {
+                        return false;
+                    }
+                }
+                linesToCheck--;
             }
-            linesToCheck--;
+            return true;
+        } finally {
+            is.reset();
         }
-        return true;
     }
 
 
