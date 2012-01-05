a3cf9ae1a988ad4de5374c9de1406de96e52864d
Lewis John McGibbney <lewismc@apache.org>
2012-01-05 12:39:50 +0000

    commit to address ANY23-22 & update to CHANGES.txt
    git-svn-id: https://svn.apache.org/repos/asf/incubator/any23/trunk@1227568 13f79535-47bb-0310-9956-ffa450edef68

--- lib/install-deps.sh
+++ lib/install-deps.sh
@@ -7,7 +7,7 @@ mvn -f ../pom.xml      \
     -Dversion=0.9.0    \
     -Dpackaging=jar    \
     -Dfile=./lib/jspf.core-0.9.0.jar \
-    -Durl=svn:https://any23.googlecode.com/svn/repo/ \
+    -Durl=svn:https://svn.apache.org/repos/asf/incubator/any23/repo/ \
 || { echo "Error while installing project dep."; exit 1; }
 
 exit 0 
