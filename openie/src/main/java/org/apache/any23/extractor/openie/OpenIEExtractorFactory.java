2f54725049f0cbc152e9e27045c0f06e93c24647
Lewis John McGibbney <lewis.mcgibbney@gmail.com>
2017-02-27 09:56:12 -0800

    ANY23-304 update package names and introduce Service Loading for OpenIE module

--- openie/src/main/java/org/apache/any23/extractor/openie/OpenIEExtractorFactory.java
+++ openie/src/main/java/org/apache/any23/extractor/openie/OpenIEExtractorFactory.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.any23.openie;
+package org.apache.any23.extractor.openie;
 
 import java.util.Arrays;
 
