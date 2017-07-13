cd8dbf0a56df58d7e2d694ce0ba8533c5745aa31
Jacek Grzebyta <grzebyta.dev@gmail.com>
2017-07-13 20:19:02 +0100

    Update values in RDFSchemaUtilsTest.java
    Signed-off-by:Jacek Grzebyta <grzebyta.dev@gmail.com>

--- core/src/test/java/org/apache/any23/vocab/RDFSchemaUtilsTest.java
+++ core/src/test/java/org/apache/any23/vocab/RDFSchemaUtilsTest.java
@@ -43,7 +43,7 @@ public class RDFSchemaUtilsTest {
      */
     @Test
     public void testSerializeVocabulariesNTriples() {
-        serializeVocabularies(RDFFormat.NTRIPLES, 2098);
+        serializeVocabularies(RDFFormat.NTRIPLES, 2102);
     }
 
     /**
@@ -53,7 +53,7 @@ public class RDFSchemaUtilsTest {
      */
     @Test
     public void testSerializeVocabulariesRDFXML() {
-        serializeVocabularies(RDFFormat.RDFXML, 5479); // Effective lines + separators.
+        serializeVocabularies(RDFFormat.RDFXML, 5489); // Effective lines + separators.
     }
 
     private void serializeVocabularies(RDFFormat format, int expectedLines) {
