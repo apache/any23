0291f588d04859053ef4eb8845686bad824b4461
Hans <firedrake93@gmail.com>
2018-08-02 13:01:19 -0500

    added license and javadoc

--- cli/src/main/java/org/apache/any23/cli/BaseTool.java
+++ cli/src/main/java/org/apache/any23/cli/BaseTool.java
@@ -1,9 +1,33 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.any23.cli;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
+/**
+ * This class reduces the verbosity of testing command-line
+ * console output by intercepting the underlying {@link PrintStream}
+ * when applicable and replacing it with a more concise version.
+ *
+ * @author Hans Brende (hansbrende@apache.org)
+ */
 abstract class BaseTool implements Tool {
 
     abstract PrintStream getOut();
